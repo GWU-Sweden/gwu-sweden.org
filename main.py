5aba4ddebc44d257fa3a4a815dcf2abe7d3f3345
Tobias Mollstam <tobias@mollstam.com>
2019-04-01 10:56:10 +0200

    added AGM 2019 invitation page

--- main.py
+++ main.py
@@ -11,3 +11,9 @@ def index():
 @app.route('/join.html')
 def join():
   return render_template('join.html')
+
+
+@app.route("/agm2019/")
+@app.route("/agm2019/index.html")
+def agm2019():
+  return render_template('agm2019.html')
